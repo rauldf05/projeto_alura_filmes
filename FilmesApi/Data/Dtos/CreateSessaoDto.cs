ead076ec634f1d2a5eb9278316270583ee2166b4
Raul Ferreira <raul.ferreira@talkcomm.com.br>
2024-05-08 09:14:06 -0300

    Feat:#0001 firs commit alura filmes

---
+++ FilmesApi/Data/Dtos/CreateSessaoDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos;
+
+public class CreateSessaoDto
+{
+    public int FilmeId { get; set; }
+    public int CinemaId { get; set; }
+}
